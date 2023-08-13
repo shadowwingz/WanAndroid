b485a1b28852759c0e6c659f0ddb0cd955186389
shadowwingz <shadowwingzhang@gmail.com>
2023-08-13 18:31:13 +0800

    Paging 列表的返回值改为 Response

--- app/src/main/java/com/shadowwingz/wanandroid/home/data/api/ArticleService.kt
+++ app/src/main/java/com/shadowwingz/wanandroid/home/data/api/ArticleService.kt
@@ -1,11 +1,12 @@
 package com.shadowwingz.wanandroid.home.data.api
 
 import com.shadowwingz.wanandroid.home.data.article.model.ArticleResponse
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ArticleService {
   
   @GET("article/list/{pageId}/json")
-  suspend fun searchRepos(@Path("pageId") page: Int): ArticleResponse
+  suspend fun searchRepos(@Path("pageId") page: Int): Response<ArticleResponse>
 }
--- app/src/main/java/com/shadowwingz/wanandroid/home/ui/article/ArticlePagingSource.kt
+++ app/src/main/java/com/shadowwingz/wanandroid/home/ui/article/ArticlePagingSource.kt
@@ -4,6 +4,9 @@ import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.shadowwingz.wanandroid.home.data.api.ArticleService
 import com.shadowwingz.wanandroid.home.data.article.model.ArticleListBean
+import com.shadowwingz.wanandroid.home.data.article.model.ArticleResponse
+import safeApiCall
+import java.io.IOException
 import javax.inject.Inject
 
 class ArticlePagingSource @Inject constructor(
@@ -21,8 +24,10 @@ class ArticlePagingSource @Inject constructor(
     // params.key 是即将加载的页面索引
     val position = params.key ?: 0
     return try {
-      val response = service.searchRepos(position)
-      val articleList = response.data.articleListBean.sortedByDescending { it.publishTime }
+      val response = search(position)
+      val articleList = response.getOrNull()?.let {
+        it.data.articleListBean.sortedByDescending { it.publishTime }
+      } ?: emptyList()
       // 如果获取的 articleList 没有数据，说明不需要再加载下一页了，此时 nextKey 设置为 null
       // 如果获取的 articleList 有数据，就把 nextKey 值在之前的基础上加 1。
       val nextKey = if (articleList.isEmpty()) {
@@ -38,5 +43,21 @@ class ArticlePagingSource @Inject constructor(
     } catch (exception: Exception) {
       return LoadResult.Error(exception)
     }
+  }
+
+  private suspend fun search(position: Int) = safeApiCall(
+    call = { requestSearch(position) },
+    errorMessage = "文章加载失败"
+  )
+
+  private suspend fun requestSearch(position: Int): Result<ArticleResponse> {
+    val response = service.searchRepos(position)
+    if (response.isSuccessful) {
+      val body = response.body()
+      if (body != null) {
+        return Result.success(body)
+      }
+    }
+    return Result.failure(IOException("${response.code()} ${response.message()}"))
   }
 }
